ed58114fb69e6897816b9cb55cb11d49129e6d76
kosinix <kosinix@users.noreply.github.com>
2016-02-07 07:03:35 +0800

    Removed register function as it is not used in our context

--- src/Simplarity/Pimple.php
+++ src/Simplarity/Pimple.php
@@ -260,22 +260,5 @@ class Pimple implements \ArrayAccess
         return array_keys($this->values);
     }
 
-    /**
-     * Registers a service provider.
-     *
-     * @param ServiceProviderInterface $provider A ServiceProviderInterface instance
-     * @param array                    $values   An array of values that customizes the provider
-     *
-     * @return static
-     */
-    public function register(ServiceProviderInterface $provider, array $values = array())
-    {
-        $provider->register($this);
-
-        foreach ($values as $key => $value) {
-            $this[$key] = $value;
-        }
-
-        return $this;
-    }
+    
 }
